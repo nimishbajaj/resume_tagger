68be416ea07be62498fe241d5f24998a265c2625
Nimish Bajaj <nimishbajaj@MacBook-Air-4.local>
2021-12-05 23:31:31 -0500

    Increased multi processing

--- resume_classifier.py
+++ resume_classifier.py
@@ -115,99 +115,100 @@ def label_freq(df, unique_labels):
                          rotation=45,
                          horizontalalignment='right')
 
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-tokenizer = Tokenizer(num_words=MAX_WORDS, lower=True)
-tokenizer.fit_on_texts(df['content'])
-sequences = tokenizer.texts_to_sequences(df['content'])
-x = pad_sequences(sequences, maxlen=MAX_LEN)
+with tf.device('/cpu:0'):
+    from tensorflow.keras.preprocessing.text import Tokenizer
+    from tensorflow.keras.preprocessing.sequence import pad_sequences
+    tokenizer = Tokenizer(num_words=MAX_WORDS, lower=True)
+    tokenizer.fit_on_texts(df['content'])
+    sequences = tokenizer.texts_to_sequences(df['content'])
+    x = pad_sequences(sequences, maxlen=MAX_LEN)
 
 
 
 
-train = df
-x_train = train["content"].str.lower()
-y_train = train[unique_labels].values
+    train = df
+    x_train = train["content"].str.lower()
+    y_train = train[unique_labels].values
 
 
 
-tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=MAX_WORDS, lower=True)
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=MAX_WORDS, lower=True)
 
-tokenizer.fit_on_texts(x_train)
+    tokenizer.fit_on_texts(x_train)
 
-x_train = tokenizer.texts_to_sequences(x_train)
-x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=MAX_LEN)
+    x_train = tokenizer.texts_to_sequences(x_train)
+    x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=MAX_LEN)
 
-embeddings_index = {}
+    embeddings_index = {}
 
-with open(GLOVE_EMBEDDING, encoding='utf8') as f:
-    for line in f:
-        values = line.rstrip().rsplit(' ')
-        word = values[0]
-        embed = np.asarray(values[1:], dtype='float32')
-        embeddings_index[word] = embed
+    with open(GLOVE_EMBEDDING, encoding='utf8') as f:
+        for line in f:
+            values = line.rstrip().rsplit(' ')
+            word = values[0]
+            embed = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = embed
 
-word_index = tokenizer.word_index
+    word_index = tokenizer.word_index
 
 
-embedding_matrix = np.zeros((MAX_WORDS, EMBEDDING_SIZE), dtype='float32')
+    embedding_matrix = np.zeros((MAX_WORDS, EMBEDDING_SIZE), dtype='float32')
 
-for word, i in word_index.items():
+    for word, i in word_index.items():
 
-    if i >= MAX_WORDS:
-        continue
+        if i >= MAX_WORDS:
+            continue
 
-    embedding_vector = embeddings_index.get(word)
+        embedding_vector = embeddings_index.get(word)
 
-    if embedding_vector is not None:
-        embedding_matrix[i] = embedding_vector
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
 
-input = tf.keras.layers.Input(shape=(MAX_LEN,))
-x = tf.keras.layers.Embedding(MAX_WORDS, EMBEDDING_SIZE, weights=[embedding_matrix], trainable=False)(input)
+    input = tf.keras.layers.Input(shape=(MAX_LEN,))
+    x = tf.keras.layers.Embedding(MAX_WORDS, EMBEDDING_SIZE, weights=[embedding_matrix], trainable=False)(input)
 
-x = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(128, return_sequences=True, dropout=0.1,
-                                                      recurrent_dropout=0.1))(x)
+    x = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(128, return_sequences=True, dropout=0.1,
+                                                          recurrent_dropout=0.1))(x)
 
-x = tf.keras.layers.Conv1D(100, kernel_size=1)(x)
+    x = tf.keras.layers.Conv1D(100, kernel_size=1)(x)
 
-avg_pool = tf.keras.layers.GlobalAveragePooling1D()(x)
-max_pool = tf.keras.layers.GlobalMaxPooling1D()(x)
+    avg_pool = tf.keras.layers.GlobalAveragePooling1D()(x)
+    max_pool = tf.keras.layers.GlobalMaxPooling1D()(x)
 
-x = tf.keras.layers.concatenate([avg_pool, max_pool])
-# x = tf.keras.layers.Dense(10, activation='relu')(x)
-preds = tf.keras.layers.Dense(len(unique_labels), activation="sigmoid")(x)
+    x = tf.keras.layers.concatenate([avg_pool, max_pool])
+    # x = tf.keras.layers.Dense(10, activation='relu')(x)
+    preds = tf.keras.layers.Dense(len(unique_labels), activation="sigmoid")(x)
 
-model = tf.keras.Model(input, preds)
+    model = tf.keras.Model(input, preds)
 
-model.summary()
+    model.summary()
 
-model.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(lr=1e-3), metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(lr=1e-3), metrics=['accuracy'])
 
-batch_size = 128
+    batch_size = 128
 
-checkpoint_path = "./training_1/cp.ckpt"
-checkpoint_dir = os.path.dirname(checkpoint_path)
+    checkpoint_path = "./training_1/cp.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
 
-cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
-                                                 save_weights_only=True,
-                                                 verbose=1)
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                     save_weights_only=True,
+                                                     verbose=1)
 
-callbacks = [
-    tf.keras.callbacks.EarlyStopping(patience=5, monitor='val_loss'),
-    tf.keras.callbacks.TensorBoard(log_dir='./logs'),
-    cp_callback
-]
+    callbacks = [
+        tf.keras.callbacks.EarlyStopping(patience=5, monitor='val_loss'),
+        tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+        cp_callback
+    ]
 
-model.fit(x_train, y_train, validation_split=0.2, batch_size=batch_size,
-          epochs=EPOCHS, callbacks=callbacks, verbose=1)
+    model.fit(x_train, y_train, validation_split=0.2, batch_size=batch_size,
+              epochs=EPOCHS, callbacks=callbacks, verbose=1)
 
-latest = tf.train.latest_checkpoint(checkpoint_dir)
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
 
-model.load_weights(latest)
+    model.load_weights(latest)
 
-predictions = model.predict(np.expand_dims(x_train[43], 0))
+    predictions = model.predict(np.expand_dims(x_train[43], 0))
 
-print(tokenizer.sequences_to_texts([x_train[43]]))
-print(y_train[43])
-print(predictions)
+    print(tokenizer.sequences_to_texts([x_train[43]]))
+    print(y_train[43])
+    print(predictions)
 
