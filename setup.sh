4ca6a69a6b47b25b84d69c57b2923f2f52368d11
Nimish Bajaj <nimishbajaj@MacBook-Air-7.local>
2021-12-09 15:27:01 -0500

    updated the setup file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 wget https://nlp.stanford.edu/data/glove.6B.zip
 wget https://github.com/florex/resume_corpus/raw/master/resumes_corpus.zip
-unzip resume_corpus
+unzip resumes_corpus
+unzip glove.6B.zip -d embedding
