4ba3f231ab0b462a99eab0c5456ed75ecc2aeedd
Nimish Bajaj <nimishbajaj@MacBook-Air-7.local>
2021-12-09 20:37:49 -0500

    updated inference with shap

--- inference.py
+++ inference.py
@@ -3,6 +3,7 @@ import utils
 import tensorflow as tf
 import numpy as np
 from config import *
+import shap
 
 
 checkpoint_dir = os.path.dirname(checkpoint_path)
@@ -41,6 +42,14 @@ x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=MAX_LEN)
 
 predictions = model.predict(np.expand_dims(x_train[43], 0))
 
+
 print(tokenizer.sequences_to_texts([x_train[43]]))
 print(y_train[43])
 print(predictions)
+
+background = x_train.iloc[np.random.choice(x_train.shape[0], 100, replace=False)]
+explainer = shap.DeepExplainer((model.layers[0].input, model.layers[-1].input), background)
+
+shap_values = explainer.shap_values(x_train[:10].values)
+
+shap.force_plot(explainer.expected_value[0], shap_values[0][0], feature_names=x_train.columns)
